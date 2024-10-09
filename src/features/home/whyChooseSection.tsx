9f3f56a2353baf237ccbe2c93e7e9a4b0604725d
Sm9090 <sameer.m@xeverse.io>
2024-10-10 03:56:08 +0500

    Fix : responsive

--- src/features/home/whyChooseSection.tsx
+++ src/features/home/whyChooseSection.tsx
@@ -2,8 +2,6 @@
 import driver from "../../../public/assets/driver.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import { Navigation } from "swiper/modules";
 
 import React from "react";
@@ -27,14 +25,13 @@ function WhyChooseSection() {
     },
   ];
   return (
-    <div>
-      <div className="mx-[100px] my-[100px] lg:mx-[32px] flex  items-center lg:flex-col lg:items-start">
+      <div className="mx-[100px] lg2:mx-[60px] my-[100px] lg:my-[70px] lg:mx-[32px] flex  items-center lg:block ">
         <div className="relative md:mb-[33px]">
-        <h1 className="text-[#FFFFFF] hidden lg:block pl-[48px] pb-[20px] font-bold font-neuePlak text-[72px] md:text-[32px] leading-[101px] lg:pl-0 lg:pb-[33px] ">
+          <h1 className="text-[#FFFFFF] hidden lg:block pl-[48px] pb-[20px] font-bold font-neuePlak text-[72px]  md:text-[32px] leading-[101px] lg:pl-0 lg:pb-[33px] ">
             Why Choose <span className="text-[#D91E27]">Us</span>
           </h1>
           <div
-            className=" w-[500px] h-[563px] md:w-[362px] md:h-[371px] mr-[63px] rounded-lg"
+            className=" w-[500px] h-[563px] md:w-[362px] md:h-[371px] mr-[63px] lg2:mr-[30px] lg:m-auto  rounded-lg"
             style={{
               backgroundImage: `url(${driver.src})`,
               backgroundRepeat: "no-repeat",
@@ -45,31 +42,40 @@ function WhyChooseSection() {
           />
           <div className="w-4 h-4 bg-[#D91E27] absolute z-10 right-[-9px] top-[150px] rounded-full lg:hidden" />
         </div>
-        <div className="">
-          <h1 className="text-[#FFFFFF] pl-[48px] pb-[20px] font-bold font-neuePlak text-[72px] leading-[101px] lg:hidden ">
+        <div className="lg:mt-[33px] " >
+          <h1 className="text-[#FFFFFF] pl-[48px] pb-[20px] font-bold font-neuePlak text-[72px] lg2:text-[50px] leading-[101px] lg:hidden ">
             Why Choose <span className="text-[#D91E27]">Us</span>
           </h1>
           <Swiper
             direction="vertical"
-            className="mySwiper  border-l !pl-[48px] lg:border-none lg:!p-0  h-[534px] lg:h-[1000px]"
+            className="mySwiper  border-l !pl-[48px] lg:border-none lg:!p-0  h-[534px] "
             spaceBetween={40}
-            slidesPerView={2}
+            slidesPerView={1.5}
             modules={[Navigation]}
             breakpoints={{
               1024: {
-                spaceBetween:40
+                spaceBetween: 40,
+              },
+              768: {
+                slidesPerView: 2.5
+              },
+              600: {
+                slidesPerView: 2
+              },
+              500:{
+                slidesPerView: 1.7
               },
               0: {
-                spaceBetween: 20
+                spaceBetween: 20,
+                slidesPerView: 1.4
               },
             }}
-            
           >
             <ul>
               {cards.map((card, index) => (
                 <SwiperSlide
                   key={index}
-                  className="bg-[#D91E27] p-8 rounded-md !max-h-max !w-[619px] lg:!w-[368px]"
+                  className="bg-[#D91E27] p-8 rounded-md  !w-[619px] lg2:!w-auto "
                 >
                   <li>
                     <div className="mb-[13px] flex items-start gap-[15px]  relative">
@@ -85,11 +91,10 @@ function WhyChooseSection() {
                 </SwiperSlide>
               ))}
             </ul>
-            <div className="absolute bottom-0 left-0 lg:bottom-[17.875rem] w-full h-[120px] bg-gradient-to-t from-black to-transparent pointer-events-none z-10"></div>{" "}
+            <div className="absolute bottom-[-3px] left-0  w-full h-[120px] bg-gradient-to-t from-black to-transparent pointer-events-none z-10"></div>{" "}
           </Swiper>
         </div>
       </div>
-    </div>
   );
 }
 
